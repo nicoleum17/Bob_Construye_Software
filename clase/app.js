3922d52f8167dfd9031f88a576559eb9cea4b75a
Nicole Uriostegui <a01711853@tec.mx>
2025-02-20 12:49:35 -0600

    [ADD] Asyncronous sort

--- clase/app.js
+++ clase/app.js
@@ -2,4 +2,15 @@
 const file_system = require("fs");
 
 // Código asincrónico, se ejecuta mientras el sistema hace otra
+// Esta es sincrónica, bloquea todo hasta que
 file_system.writeFileSync("hola.txt", "Hola desde node");
+
+// Asyncronous sort
+const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
+
+for (let item of arreglo) {
+  //Los imprime ordenados debido a que los imprime en función de su valor
+  setTimeout(() => {
+    console.log(item);
+  }, item);
+}
