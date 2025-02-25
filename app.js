e8529e5deee4acf1d227b5c37a3c772285fb0a3e
Nicole Uriostegui <a01711853@tec.mx>
2025-02-25 13:02:23 -0600

    [MOD] app.js

--- app.js
+++ app.js
@@ -7,9 +7,16 @@ app.use((request, response, next) => {
   next(); //Le permite a la petición avanzar hacia el siguiente middleware
 });
 
+// El orden de los middleware importa
+// De lo especifico a lo general
+// app.get para solo peticiones de tipo GET
+app.get("/plantas/agregar", (request, response, next) => {
+  response.send(html_header, html_form, html_footer);
+});
+
 app.use((request, response, next) => {
   console.log("Otro middleware!");
-  response.send("¡Hola mundo!"); //Manda la respuesta
+  response.send("¡Hola mundo!"); //Manda la respuesta (return)
 });
 
 app.listen(3000);
