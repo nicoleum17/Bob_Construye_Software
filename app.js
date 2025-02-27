fcc93f4724fc0e8728e71b1b623c26e156a9b3ae
Nicole Uriostegui <a01711853@tec.mx>
2025-02-26 21:06:39 -0600

    [MOD] Lab 11

--- app.js
+++ app.js
@@ -5,25 +5,22 @@ const app = express();
 app.use(express.static("public"));
 
 // Middleware para procesar JSON y formularios
-app.use(express.json()); // 📌 ¡Esto soluciona el problema de POST!
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Middleware global de ejemplo
+//Ejemplo
 app.use((request, response, next) => {
   console.log("Middleware");
   next();
 });
 
-// 📌 Importar rutas
+//rutas
 const productosRoutes = require("./routes/productos.routes");
-
-// 📌 Usar rutas
 app.use("/productos", productosRoutes);
 
-// 📌 Middleware final: Manejo de rutas no encontradas
+//Rutas que estan mal
 app.use((request, response) => {
   response.status(404).send("<h1>Página no encontrada</h1>");
 });
 
-// Servidor en puerto 3000
 app.listen(3000);
