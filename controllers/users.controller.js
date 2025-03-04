916ded9385cae6e7162ddc8b70b229e090737e93
Nicole Uriostegui <a01711853@tec.mx>
2025-03-04 12:48:15 -0600

    [MOD] Lab14 para que aparexca y no el boton de log in

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,9 +1,13 @@
 exports.get_login = (request, response, next) => {
-  response.render("login.ejs");
+  response.render("login.ejs", {
+    // falsif - valor vacío que pasa como falso
+    isLoggedIn: request.session.isLoggedIn || false,
+  });
 };
 
 exports.post_login = (request, response, next) => {
-  console.log(request.body.username);
-  //request.session.username = request.body.username;
+  //console.log(request.body.username);
+  request.session.isLoggedIn = true;
+  request.session.username = request.body.username;
   response.redirect("/plantas");
 };
